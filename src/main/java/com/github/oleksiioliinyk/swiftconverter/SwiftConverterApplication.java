82137b4c26d016414d43273b54b138751781e5a2
RocketSpace2 <141578067+RocketSpace2@users.noreply.github.com>
2025-04-16 11:04:27 +0200

    Added project from Spring Initializr

---
+++ src/main/java/com/github/oleksiioliinyk/swiftconverter/SwiftConverterApplication.java
@@ -0,0 +1,13 @@
+package com.github.oleksiioliinyk.swiftconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/oleksiioliinyk/swiftconverter/SwiftConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oleksiioliinyk.swiftconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
